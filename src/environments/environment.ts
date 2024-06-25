fdb66bb9dbbf08c81b277a054515afc6798190a7
Enio Amarantes <37565576+EnioAmarantes@users.noreply.github.com>
2024-06-25 01:46:30 -0300

    Delete src/environments directory
    Removido para preservar chaves

--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: false,
-    googleSheets: {
-        sheetId: '15gsj8tQr8kFB14CGbgwGU_mFoL60LGNfzNRY3wwKJeM',
-        apiKey: 'AIzaSyDnQrIoVNOAB0ZqQLGd-qMZ3JM_4xlOABg',
-    }
-};
